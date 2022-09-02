968ff75b6d3f2bfe5ad8789196587cf7fba8eb65
muttakin1991 <shawonmuttakin@gmail.com>
2022-09-03 02:18:01 +0600

    category done

--- js/app.js
+++ js/app.js
@@ -25,8 +25,9 @@ const displayCategory = (categories) => {
 }
 
 const categoryNews = (id) => {
-    // console.log(id)
+    toggleSpinner(true);
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    console.log(id)
     fetch(url)
         .then(res => res.json())
         .then(data => displayCategoryNews(data.data))
@@ -37,7 +38,9 @@ const categoryNews = (id) => {
 }
 
 const displayCategoryNews = (categoryNews) => {
-    console.log(categoryNews)
+    
+    const categoryCount = document.getElementById('category-count');
+    categoryCount.innerText = categoryNews.length;
 
     const categoryNewsContainer = document.getElementById('category-news-container');
     categoryNewsContainer.innerHTML = ``;
@@ -50,40 +53,102 @@ const displayCategoryNews = (categoryNews) => {
             </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text">${news.details.length > 200 ? news.details.substring(0, 200) + '...' : news.details}</p>
                         <div class="row">
                             <div class="col-sm-3 d-flex align-items-center">
                                 <div class="w-25 rounded-circle">
-                                    <img src="./other/tree-736885__480.jpg"
+                                    <img src="${news.author.img ? news.author.img : 'not available'}"
                                         class="img-fluid  rounded-circle" alt="">
                                 </div>
                                 <div class="ps-2">
-                                    <span class="author-name">this is name</span>
+                                    <span class="author-name">${news.author.name ? news.author.name : 'not available'}</span>
                                     <br>
-                                    <span class="blog-publish-date">23-2435-235</span>
+                                    <span class="blog-publish-date">${news.author.published_date ? news.author.published_date : 'not available'}</span>
                                 </div>
                             </div>
                             <div class="col-sm-3 d-flex justify-content-center align-items-center">
-                                <i class="fa-regular fa-eye"></i> <span class="ps-2">1.5M</span>
+                                <i class="fa-regular fa-eye"></i> <span class="ps-2">${news.total_view ? news.total_view : 'not available'}</span>
                             </div>
                             <div class="col-sm-3 d-flex justify-content-center align-items-center">
                                 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
                                     class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
                             </div>
                             <div class="col-sm-3 d-flex justify-content-end align-items-center">
-                                <i class="fa-solid fa-arrow-right"></i>
+                            <a type="button" onclick="loadNewsDetails('${news._id}')" data-bs-toggle="modal" data-bs-target="#phoneDetailsModal">
+                                 <i class="fa-solid fa-arrow-right"></i>
+                            </a>
                             </div>
                         </div>
                     </div>
                 </div>
         `
         categoryNewsContainer.appendChild(newsDiv);
-    })
+    });
+    toggleSpinner(false);
 
 }
 
+const loadNewsDetails = (newsId) => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
+    console.log(newsId)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data[0]))
+        .catch((error) => {
+            console.log(error)
+        });
+}
+
+const displayNewsDetails = (news) => {
+
+    const newsDetails = document.getElementById('news-details');
+    newsDetails.innerHTML = ``;
+
+    const newsDiv = document.createElement('div');
+    newsDiv.classList.add('row');
+    newsDiv.classList.add('row', 'g-0', 'mb-3', 'border', 'rounded');
+    newsDiv.innerHTML = `
+            <div class="col-sm-12 text-center">
+                <img src="${news.thumbnail_url}" class="img-fluid" alt="...">
+            </div>
+                <div class="col-sm-12">
+                    <div class="card-body">
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text">${news.details} </p>
+                        <div class="row">
+                            <div class="col-sm-6 d-flex align-items-center">
+                                <div class="w-25 rounded-circle">
+                                    <img src="${news.author.img ? news.author.img : 'not available'}"
+                                        class="img-fluid  rounded-circle" alt="">
+                                </div>
+                                <div class="ps-2">
+                                    <span class="author-name">${news.author.name ? news.author.name : 'not available'}</span>
+                                    <br>
+                                    <span class="blog-publish-date">${news.author.published_date ? news.author.published_date : 'not available'}</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-3 d-flex justify-content-center align-items-center">
+                                // <i class="fa-regular fa-eye"></i> <span class="ps-2">${news.total_view ? news.total_view : 'not available'}</span>
+                            </div>
+                            <div class="col-sm-3 d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
+                                    class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `
+    newsDetails.appendChild(newsDiv);
+}
+
+const toggleSpinner = (isLoading) => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    } else {
+        loaderSection.classList.add('d-none');
+    }
+}
 
 category()
